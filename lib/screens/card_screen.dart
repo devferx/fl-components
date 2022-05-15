05da601989b9f88a7499bd034f862e1f4c74697a
devferx <ferkki@hotmail.com>
2022-05-15 15:59:48 -0400

    Argumentos a los Widgets

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -16,11 +16,14 @@ class CardScreen extends StatelessWidget {
         children: const [
           CustomCardType1(),
           SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(imageUrl: "https://picsum.photos/2048/1365"),
           SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(imageUrl: "https://picsum.photos/500/260"),
           SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(
+            imageUrl: "https://picsum.photos/600/260",
+            name: "The Card",
+          ),
           SizedBox(height: 100),
         ],
       ),
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,7 +2,11 @@ import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({Key? key}) : super(key: key);
+  final String imageUrl;
+  final String? name;
+
+  const CustomCardType2({Key? key, required this.imageUrl, this.name})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,19 +19,20 @@ class CustomCardType2 extends StatelessWidget {
         shadowColor: AppTheme.primary.withOpacity(0.5),
         child: Column(
           children: [
-            const FadeInImage(
-              image: NetworkImage("https://picsum.photos/2048/1365"),
-              placeholder: AssetImage("assets/jar-loading.gif"),
+            FadeInImage(
+              image: NetworkImage(imageUrl),
+              placeholder: const AssetImage("assets/jar-loading.gif"),
               width: double.infinity,
               height: 260,
               fit: BoxFit.cover,
-              fadeInDuration: Duration(milliseconds: 300),
+              fadeInDuration: const Duration(milliseconds: 300),
             ),
-            Container(
-              alignment: AlignmentDirectional.centerEnd,
-              padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
-              child: const Text("Un hermoso paisaje"),
-            )
+            if (name != null)
+              Container(
+                alignment: AlignmentDirectional.centerEnd,
+                padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+                child: Text(name!),
+              )
           ],
         ));
   }
